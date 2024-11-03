1abd43f4e7a1590df55ce7e9593ee8c44ad24678
Petr Kracik <petrkr@petrkr.net>
2024-11-03 19:28:00 +0100

    Added variant Doit devkit with 1W E22 module

---
+++ variants/DOIT_DIY_1W/board_pinout.h
@@ -0,0 +1,11 @@
+#undef HAS_DISPLAY
+
+#define RADIO_SCLK_PIN      4
+#define RADIO_MISO_PIN      2
+#define RADIO_MOSI_PIN      15
+#define RADIO_CS_PIN        5
+#define RADIO_DIO1_PIN      34
+#define RADIO_RST_PIN       -1
+#define RADIO_BUSY_PIN      39
+#define RADIO_RXEN          17
+#define RADIO_TXEN          16
