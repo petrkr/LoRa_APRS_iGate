01c5ee191c4ee296137a279135b363d562172cab
Petr Kracik <petrkr@petrkr.net>
2026-02-03 19:24:56 +0100

    NetworkManager: NM log prefix for ETH stuff

--- src/network_manager.cpp
+++ src/network_manager.cpp
@@ -82,25 +82,25 @@ void NetworkManager::_processAPTimeout() {
 void NetworkManager::_onNetworkEvent(arduino_event_id_t event, arduino_event_info_t /*info*/) {
     switch (event) {
         case ARDUINO_EVENT_ETH_START:
-            Serial.println("ETH Started");
+            Serial.println("[NM] ETH Started");
             if (!_hostName.isEmpty()) {
-                Serial.println("ETH Setting Hostname: " + _hostName);
+                Serial.println("[NM] ETH Setting Hostname: " + _hostName);
                 ETH.setHostname(_hostName.c_str());
             }
         break;
         case ARDUINO_EVENT_ETH_CONNECTED:
-            Serial.println("ETH Connected");
+            Serial.println("[NM] ETH Connected");
             break;
         case ARDUINO_EVENT_ETH_GOT_IP:
-            Serial.println("ETH Got IP");
+            Serial.println("[NM] ETH Got IP");
             _ethernetConnected = true;
             break;
         case ARDUINO_EVENT_ETH_DISCONNECTED:
-            Serial.println("ETH Disconnected");
+            Serial.println("[NM] ETH Disconnected");
             _ethernetConnected = false;
             break;
         case ARDUINO_EVENT_ETH_STOP:
-            Serial.println("ETH Stopped");
+            Serial.println("[NM] ETH Stopped");
             _ethernetConnected = false;
             break;
         default:
@@ -265,7 +265,7 @@ String NetworkManager::getWiFimacAddress(void) const {
 // Ethernet methods
 bool NetworkManager::ethernetConnect(eth_phy_type_t type, uint8_t phy_addr, uint8_t mdc, uint8_t mdio, int power, eth_clock_mode_t clock_mode, bool use_mac_from_efuse) {
     _ethernetMode = true;
-    Serial.println("Setting up Ethernet...");
+    Serial.println("[NM] Setting up Ethernet...");
 
     #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
         // SDK 5.x (Arduino SDK 3.x)
@@ -285,7 +285,7 @@ bool NetworkManager::setEthernetIP(const String& staticIP, const String& gateway
 
     IPAddress ip, gw, sn, d1, d2;
     if (!ip.fromString(staticIP) || !gw.fromString(gateway) || !sn.fromString(subnet)) {
-        Serial.println("Invalid static IP configuration");
+        Serial.println("[NM] Invalid static IP configuration");
         return false;
     }
 
@@ -299,7 +299,7 @@ bool NetworkManager::setEthernetIP(const String& staticIP, const String& gateway
         ETH.config(ip, gw, sn);
     }
 
-    Serial.println("Ethernet static IP: " + staticIP);
+    Serial.println("[NM] Ethernet static IP: " + staticIP);
     return true;
 }
 
