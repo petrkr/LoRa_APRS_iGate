07973a2a7606f5958d969c05c59fee99a4d0f7e6
Petr Kracik <petrkr@petrkr.net>
2026-02-03 16:58:02 +0100

    ETH class: Supports static IP

--- src/eth_utils.cpp
+++ src/eth_utils.cpp
@@ -1,35 +1,59 @@
 #include <ETH.h>
 #include "configuration.h"
 #include "network_manager.h"
-#include "wifi_utils.h"
 #include "display.h"
-#include "utils.h"
+#include "board_pinout.h"
 
 extern Configuration    Config;
 extern NetworkManager   *networkManager;
 
-uint8_t                 ethCounter = 0;
-
 namespace ETH_Utils {
-    void startEthernet() {
+
+    bool setup() {
+        if (!Config.ethConfig.enabled) {
+            Serial.println("\nEthernet is disabled");
+            return false;
+        }
+
         displayShow("", "Connecting to LAN", "", " ...", 0);
         Serial.println("\nConnecting to LAN...");
 
-        networkManager->ethernetConnect(ETH_PHY_LAN8720, 1, 23, 18, -1, ETH_CLOCK_GPIO17_OUT);
+        networkManager->ethernetConnect(
+            ETH_PHY_TYPE,
+            LAN_PHY_ADDR,
+            LAN_MDC_PIN,
+            LAN_MDIO_PIN,
+            LAN_POWER_PIN,
+            LAN_CLOCK_MODE,
+            false  // useMacFromEfuse
+        );
+
+        // Configure static IP if enabled
+        if (Config.ethConfig.useStaticIP) {
+            networkManager->setEthernetIP(
+                Config.ethConfig.staticIP,
+                Config.ethConfig.gateway,
+                Config.ethConfig.subnet,
+                Config.ethConfig.dns1,
+                Config.ethConfig.dns2
+            );
+        }
+
+        unsigned long startMillis = millis();
+        int timeoutSec = 15;
 
         while (!networkManager->isEthernetConnected()) {
-            delay(1000);
+            delay(500);
             Serial.print(".");
-            ethCounter++;
-            if (ethCounter > 10) {
-                Serial.println("\nEthernet connection failed");
-                displayShow("", "Ethernet connection", "failed", " ...", 0);
-                return;
+            if (millis() - startMillis > timeoutSec * 1000) {
+                Serial.println("\nEthernet connection timeout");
+                displayShow("", "Ethernet timeout", "", "", 2000);
+                return false;
             }
         }
-    }
 
-    void setup() {
-        startEthernet();
+        Serial.println("\nEthernet connected! IP: " + networkManager->getEthernetIP().toString());
+        displayShow("", "LAN Connected!", networkManager->getEthernetIP().toString(), "", 2000);
+        return true;
     }
 }
